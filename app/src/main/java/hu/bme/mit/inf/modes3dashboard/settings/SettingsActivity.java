fbcb3a20b0c9a72460a5b0d57af5486388a65fd2
MyName <mail@email.com>
2018-12-11 14:03:50 +0100

    SettingsActivity shared preferences initialize

--- app/src/main/java/hu/bme/mit/inf/modes3dashboard/settings/SettingsActivity.java
+++ app/src/main/java/hu/bme/mit/inf/modes3dashboard/settings/SettingsActivity.java
@@ -1,11 +1,16 @@
 package hu.bme.mit.inf.modes3dashboard.settings;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import hu.bme.mit.inf.modes3dashboard.R;
 
@@ -18,14 +23,33 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onSave();
             }
         });
+
+        onLoad();
+    }
+    private void onSave(){
+        EditText serverUriEditText = (EditText) findViewById(R.id.serverUriEditText);
+        String serverURI = serverUriEditText.getText().toString();
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("ServerURI", serverURI);
+        editor.commit();
+    }
+    private void onLoad(){
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = getResources().getString(R.string.settings_mqttURI_spaceholder);
+        String serverURI = sharedPref.getString("ServerURI", defaultValue);
+
+        EditText serverUriEditText = (EditText) findViewById(R.id.serverUriEditText);
+        serverUriEditText.setText(serverURI);
+
     }
 
 }
