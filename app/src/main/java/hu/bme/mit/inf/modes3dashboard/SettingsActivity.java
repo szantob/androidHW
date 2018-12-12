d882671b970d00fdd46e9c10da257da251d0b6b6
MyName <mail@email.com>
2018-12-12 20:05:01 +0100

    settings with sharedpreferences

--- app/src/main/java/hu/bme/mit/inf/modes3dashboard/SettingsActivity.java
+++ app/src/main/java/hu/bme/mit/inf/modes3dashboard/SettingsActivity.java
@@ -1,14 +1,26 @@
 package hu.bme.mit.inf.modes3dashboard;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    EditText serverEditText;
+    EditText portEditText;
+    Button saveAndConnectButton;
+    EditText subTopicEditText;
+    EditText pubTopicEditText;
+    EditText pubNameEditText;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,61 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        serverEditText = findViewById(R.id.editTextServer);
+        portEditText = findViewById(R.id.editTextPort);
+        saveAndConnectButton = findViewById(R.id.connectButton);
+        subTopicEditText = findViewById(R.id.editTextSubTopic);
+        pubTopicEditText = findViewById(R.id.editTextPubTopic) ;
+        pubNameEditText = findViewById(R.id.editTextPubName) ;
+        saveButton = findViewById(R.id.saveButton);
+
+        saveAndConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mqttSaveAndConnect();
+            }
+        });
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                settingsSave();
+            }
+        });
+        initializeSettings();
     }
 
+    private void initializeSettings(){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String serverDefaultValue = getResources().getString(R.string.server_input_placeholder);
+        String portDefaultValue = getResources().getString(R.string.port_input_placeholder);
+        String subTopicDefaultValue = getResources().getString(R.string.topic_name);
+        String pubTopicDefaultValue = getResources().getString(R.string.topic_name);
+        String pubNameDefaultValue = getResources().getString(R.string.name);
+
+        serverEditText.setText(sharedPref.getString(getString(R.string.MQTTServer), serverDefaultValue));
+        portEditText.setText(sharedPref.getString(getString(R.string.MQTTPort), portDefaultValue));
+        subTopicEditText.setText(sharedPref.getString(getString(R.string.MQTTSubTpoic), subTopicDefaultValue));
+        pubTopicEditText.setText(sharedPref.getString(getString(R.string.MQTTPubTpoic), pubTopicDefaultValue));
+        pubNameEditText.setText(sharedPref.getString(getString(R.string.MQTTPubName), pubNameDefaultValue));
+    }
+
+    private void mqttSaveAndConnect(){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.MQTTServer), serverEditText.getText().toString());
+        editor.putString(getString(R.string.MQTTPort), portEditText.getText().toString());
+        editor.apply();
+
+    }
+    private void settingsSave(){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.MQTTServer), serverEditText.getText().toString());
+        editor.putString(getString(R.string.MQTTPort), portEditText.getText().toString());
+        editor.putString(getString(R.string.MQTTSubTpoic), subTopicEditText.getText().toString());
+        editor.putString(getString(R.string.MQTTPubTpoic), pubTopicEditText.getText().toString());
+        editor.putString(getString(R.string.MQTTPubName), pubNameEditText.getText().toString());
+        editor.apply();
+    }
 }
